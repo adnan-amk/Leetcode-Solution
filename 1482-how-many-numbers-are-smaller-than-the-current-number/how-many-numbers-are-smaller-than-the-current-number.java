3fcc89c8a8304eacdda981f876469b3a525168d6
Adnan Maqsood Khan <76204115+kd3-80115@users.noreply.github.com>
2023-10-27 18:55:28 +0530

    Time: 1 ms (100.00%) | Memory: 43.4 MB (73.19%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,17 +1,25 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-    int size=nums.length;
-    int[] arr=new int[size];
-    for(int i=0;i<size;i++)
-    {
-         int count=0;
-         for(int j=0;j<size;j++)
-         {
-             if(nums[i]>nums[j])
-                {count++;}
-         }
-         arr[i]=count;
-     }
-     return arr;
+    int[] counts = new int[101]; // Since the constraints say 0 <= nums[i] <= 100
+        int[] result = new int[nums.length];
+        
+        // Count the occurrences of each number
+        for (int num : nums) {
+            counts[num]++;
+        }
+        
+        // Calculate the cumulative sum of counts
+        for (int i = 1; i < counts.length; i++) {
+            counts[i] += counts[i - 1];
+        }
+        
+        // Populate the result array
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result[i] = counts[nums[i] - 1];
+            }
+        }
+        
+        return result;
     }
 }
