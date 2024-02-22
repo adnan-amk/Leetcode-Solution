3afcab5c3c47bb8d725a48256dff7bd08f537088
Adnan Maqsood Khan <76204115+kd3-80115@users.noreply.github.com>
2024-02-22 21:08:13 +0530

    Time: 3 ms (87.42%) | Memory: 43.9 MB (58.78%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -1,17 +1,14 @@
 class Solution {
-    public int mostWordsFound(String[] s) {
-     int max=Integer.MIN_VALUE;
-     for(int i=0;i<s.length;i++)
-     {
-         int count=1;
-         for(int j=0;j<s[i].length();j++)
-         {
-             if(s[i].charAt(j)==' ')
-                count++;
-         }
-         if(count>max)
-            max=count;
-     }   
-     return max;
+    public int mostWordsFound(String[] sentences) {
+        int len=Integer.MIN_VALUE;
+        for(int i=0;i<sentences.length;i++)
+        {
+            int temp=sentences[i].split(" ").length;
+            if(temp>len)
+            {
+                 len=temp;
+            }
+        }
+        return len;
     }
 }
