7283095fee37bda3efc6a7623e32407c865691d8
Adnan Maqsood Khan <76204115+adnan-amk@users.noreply.github.com>
2024-07-23 21:51:07 +0530

    Time: 6 ms (75.35%) | Memory: 44.2 MB (82.12%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -7,9 +7,13 @@ class Solution {
         {
             freqMap.put(num,freqMap.getOrDefault(num,0)+1);
         }
-        Integer temp[]=new Integer[len];
-        for(int i=0;i<len;i++)
-            temp[i]=nums[i];
+        Integer temp[]=new Integer[freqMap.size()];
+        int i=0;
+        for(Integer key:freqMap.keySet())
+        {
+            temp[i]=key;
+            i++;
+        }
 
         //create a custom comparator to compare values based on freq    
         Arrays.sort(temp, (a,b)->{
@@ -17,8 +21,17 @@ class Solution {
                     return b-a;
                 return freqMap.get(a)-freqMap.get(b); //inc order of value
         });
-        for(int i=0;i<len;i++)
-            nums[i]=temp[i];
+        
+        i=0;
+        for(int key:temp)
+        {
+            int f=  freqMap.get(key);
+            for(int j=0;j<f;j++)
+            {
+                nums[i]=key;
+                i++;
+            }
+        }
         return nums;
     }
 }
