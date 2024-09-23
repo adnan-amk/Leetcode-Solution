8f5684d2190555c569fa27fd99861fe3aa6b9b15
Adnan Maqsood Khan <76204115+adnan-amk@users.noreply.github.com>
2024-09-23 15:45:30 +0530

    Time: 8 ms (31.64%) | Memory: 41 MB (40.19%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int count=0;
+        int row=1;
+        while(n>=row){
+            n-=row;
+            row++;
+            count++;
+        }
+        return count;
+    }
+}
