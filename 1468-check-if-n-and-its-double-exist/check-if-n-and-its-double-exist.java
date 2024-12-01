9433a7d875ed3db43abc0e02a018c5bc49c0c33a
Adnan Maqsood Khan <76204115+adnan-amk@users.noreply.github.com>
2024-12-01 21:37:23 +0530

    Time: 3 ms (33.07%) | Memory: 42.9 MB (42.13%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,11 +1,18 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        /*Brutre force approach*/
+        /*Brutre force approach
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
                 if(arr[i]==arr[j]*2 && i!=j)
                     return true;
             }
+        }*/
+        /*Better aproach*/
+        Set<Integer> seen = new HashSet<>();
+        for(int e : arr){
+            if(seen.contains(e*2) || (e%2==0 && seen.contains(e/2)))
+                return true;
+            seen.add(e);
         }
         return false;
     }
